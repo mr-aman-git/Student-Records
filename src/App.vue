4ea1482ff9124ceffd1601325a1adf701f2baca6
mr-aman-git <amanazr2003@gmail.com>
2025-02-23 17:03:34 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import Student from "./components/Student.vue";
+export default{
+  name: 'App',
+  components{
+    name:'Student'
+  }
+
+}
+
+</script>
